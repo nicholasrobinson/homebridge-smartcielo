a52a5f6359a7674a36743ebb8e6fd0f88bb5e6ef
Nicholas Robinson <nicholasrobinson@NicholasMacBook.localdomain>
2020-05-17 20:39:09 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,29 +1,70 @@
-# homebridge-dummy-thermostat
-Plugin for homebridge to create a fake thermostat accessory
-## Installation
+# NodeJs interface for smartcielo remote AC control API (MRCOOL)
 
-1. Install [homebridge](https://github.com/nfarina/homebridge#installation-details)
-2. Install this plugin: `npm install -g homebridge-dummy-thermostat`
-3. Edit your `config.json` file (See below).
+by Nicholas Robinson
 
-## Configuration example
+Smartcielo (MRCOOL) plug-in for [Homebridge](https://github.com/nfarina/homebridge) using the smartcielo API.
+
+Integrate your MRCOOL AC into your HomeKit system.
+
+[![mit license](https://badgen.net/badge/license/MIT/red)](https://github.com/nicholasrobinson/homebridge-smartcielo/blob/master/LICENSE)
+[![npm](https://badgen.net/npm/v/homebridge-smartcielo)](https://www.npmjs.com/package/homebridge-smartcielo)
+[![npm](https://badgen.net/npm/dt/homebridge-smartcielo)](https://www.npmjs.com/package/homebridge-smartcielo)
+
+Forked from:
+https://github.com/X1ZOR/homebridge-dummy-thermostat
+
+With inspiration from:
+https://github.com/chrisjshull/homebridge-nest
+
+# Installation
+
+<!-- 2. Clone (or pull) this repository from github into the same path Homebridge lives (usually `/usr/local/lib/node_modules`). Note: the code currently on GitHub is in beta, and is newer than the latest published version of this package on `npm` -->
+1. Install homebridge using: `npm install -g homebridge`
+2. Install this plug-in using: `npm install -g homebridge-smartcielo`
+3. Update your configuration file. See example `config.json` snippet below.
+
+# Configuration
+
+Configuration sample (edit `~/.homebridge/config.json`):
 
 ```json
 "accessories": [
      {
-       "accessory": "Thermostat",
-       "name": "Thermostat"
+       "accessory": "SmartCielo",
+       "name": "SmartCielo",
+       "username": "<SMARTCIELO_USERNAME>",
+       "password": "<SMARTCIELO_PASSWORD>",
+       "ip": "<PUBLIC_IP_ADDRESS>",
      }
 ]
 ```
 
-### Structure
+## Structure
 
 | Key | Description |
 | --- | --- |
-| `accessory` | Must be `Thermostat` |
+| `accessory` | Must be `SmartCielo` |
 | `name` | Name to appear in the Home app |
-| `temperatureDisplayUnits` _(optional)_ | Whether you want °C (`0`) or °F (`1`) as your units (`0` is default) |
+| `username` | smartcielo.com username |
+| `password` | smartcielo.com password |
+| `ip` | public ip address where ac resides |
+| `temperatureDisplayUnits` _(optional)_ | Whether you want °C (`0`) or °F (`1`) as your units (`1` is default) |
 | `model` _(optional)_ | Appears under "Model" for your accessory in the Home app |
 | `manufacturer` _(optional)_ | Appears under "Manufacturer" for your accessory in the Home app |
 
+# Things to try with Siri
+
+* Hey Siri, *set the temperature to 72 degrees*. (in heat-only or cool-only mode)
+* Hey Siri, *set the temperature range to between 65 and 70 degrees*. (in auto mode, for systems that can heat and cool)
+* Hey Siri, *set the thermostat to cool*. (try heat, cool, auto, or off)
+* Hey Siri, *turn on the air conditioning*.
+* Hey Siri, *what's the temperature at home*?
+* Hey Siri, *what's the temperature in the Basement*? (get the temperature from a Nest Temperature Sensor)
+
+Please let me know if you find this useful or come up with any novel implementations.
+
+Enjoy!
+
+Nicholas Robinson
+
+me@nicholassavilerobinson.com
--- index.js
+++ index.js
@@ -1,97 +1,111 @@
-var Service, Characteristic, HomnebridgeAPI;
+var SmartCielo, Service, Characteristic, HomebridgeAPI;
 
 module.exports = function (homebridge) {
+    SmartCielo = require('node-smartcielo');
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
-    HomnebridgeAPI = homebridge;
-    homebridge.registerAccessory("homebridge-dummy-thermostat", "Thermostat", Thermostat);
+    HomebridgeAPI = homebridge;
+    homebridge.registerAccessory('homebridge-smartcielo', 'SmartCieloThermostat', Thermostat);
 };
 
 function Thermostat(log, config) {
     this.log = log;
-
     this.name = config.name;
-    this.manufacturer = config.manufacturer || "DefaultManufacturer";
-    this.model = config.model || "DefaultModel";
-
-    this.temperatureDisplayUnits = config.temperatureDisplayUnits || 0;
-
-    this.cacheDir = HomnebridgeAPI.user.persistPath();
-    this.storage = require('node-persist');
-    this.storage.initSync({dir: this.cacheDir, forgiveParseErrors: true});
-
-    this.log(this.name);
+    this.manufacturer = config.manufacturer || 'DefaultManufacturer';
+    this.model = config.model || 'DefaultModel';
+    this.temperatureDisplayUnits = config.temperatureDisplayUnits || 1;
+    this.minTemperature = config.maxTemperature || 62;
+    this.maxTemperature = config.maxTemperature || 86;
+    this.commandDelay = config.commandDelay || 5;
     this.service = new Service.Thermostat(this.name);
+    this.hvac = new SmartCielo(config.username, config.password, config.ip,
+        commandedState => {
+            this.log.debug('Commanded State Change:', JSON.stringify(commandedState));
+        },
+        roomTemperature => {
+            this.log.debug('Updated Room Temperature:', roomTemperature);
+        });
 }
 
-Thermostat.prototype.getCurrentHeatingCoolingState = function(cb) {
-    this.currentHeatingCoolingState = this.storage.getItemSync(this.name + '&' + 'CurrentHeatingCoolingState');
-    if (this.currentHeatingCoolingState === undefined) {
-        this.currentHeatingCoolingState = 0;
-        this.storage.setItemSync(this.name + '&' + 'CurrentHeatingCoolingState', 0);
-    }
-	this.log(this.currentHeatingCoolingState);
-    cb(null, this.currentHeatingCoolingState);
+Thermostat.prototype.getCurrentHeatingCoolingState = function (cb) {
+    const mode = getModeHelper(this.hvac.getPower(), this.hvac.getMode());
+    this.log.debug('getCurrentHeatingCoolingState', mode);
+    const state = convertModeToHeatingCoolingState(mode);
+    cb(null, state);
 };
 
-Thermostat.prototype.getTargetHeatingCoolingState = function(cb) {
-    this.targetHeatingCoolingState = this.storage.getItemSync(this.name + '&' + 'TargetHeatingCoolingState');
-    if (this.targetHeatingCoolingState === undefined) {
-        this.targetHeatingCoolingState = 0;
-        this.storage.setItemSync(this.name + '&' + 'TargetHeatingCoolingState', 0);
-    }
-	this.log(this.targetHeatingCoolingState);
-    cb(null, this.targetHeatingCoolingState);
+Thermostat.prototype.getTargetHeatingCoolingState = function (cb) {
+    const mode = getModeHelper(this.hvac.getPower(), this.hvac.getMode());
+    this.log.debug('getTargetHeatingCoolingState', mode);
+    cb(null, convertModeToHeatingCoolingState(mode));
 };
 
-Thermostat.prototype.setTargetHeatingCoolingState = function(val, cb) {
-	this.log(val);
-    this.storage.setItemSync(this.name + '&' + 'TargetHeatingCoolingState', val);
-	this.storage.setItemSync(this.name + '&' + 'CurrentHeatingCoolingState', val);
-    this.service.setCharacteristic(Characteristic.CurrentHeatingCoolingState, val);
+Thermostat.prototype.setTargetHeatingCoolingState = function (state, cb) {
+    const mode = convertHeatingCoolingStateToMode(state);
+    this.log.debug('setTargetHeatingCoolingState', mode);
+    if (mode == 'off') {
+        this.hvac.sendPowerOff(_ => {
+            this.log.debug('Sent Command', 'sendPowerOff');
+        }, err => {
+            this.log.error('sendPowerOff Error', err);
+        });
+    } else {
+        this.hvac.sendMode(mode, _ => {
+            this.log.debug('Sent Command', 'sendMode', mode);
+            // TODO: Investigate closing the loop and removing hard-coded delay.
+            if (this.hvac.getPower() === 'off') {
+                // Note: Potentially there is a way to pass a delay through to the underlying socket queue.
+                setTimeout(() => {
+                    this.hvac.sendPowerOn(_ => {
+                        this.log.debug('Sent Command', 'sendPowerOn');
+                    }, err => {
+                        this.log.error('sendPowerOn Error', err);
+                    });
+                }, this.commandDelay * 1000);
+            }
+        }, err => {
+            this.log.error('sendMode Error', err);
+        });
+    }
     cb();
 };
 
-Thermostat.prototype.getCurrentTemperature = function(cb) {
-    this.currentTemperature = this.storage.getItemSync(this.name + '&' + 'CurrentTemperature');
-    if (this.currentTemperature === undefined) {
-        this.currentTemperature = 20;
-        this.storage.setItemSync(this.name + '&' + 'CurrentTemperature', 20);
-    }
-	this.log("Current"+this.currentTemperature);
-	cb(null, this.currentTemperature);
+Thermostat.prototype.getCurrentTemperature = function (cb) {
+    const temperature = this.hvac.getRoomTemperature();
+    this.log.debug('getCurrentTemperature', temperature);
+    cb(null, convertFahrenheitToCelsius(temperature));
 };
 
-Thermostat.prototype.getTargetTemperature = function(cb) {
-    this.targetTemperature = this.storage.getItemSync(this.name + '&' + 'TargetTemperature');
-    if (this.targetTemperature === undefined) {
-        this.targetTemperature = 20;
-        this.storage.setItemSync(this.name + '&' + 'TargetTemperature', 20);
-    }
-	this.log("Target"+this.targetTemperature);
-	cb(null, this.targetTemperature);
+Thermostat.prototype.getTargetTemperature = function (cb) {
+    const temperature = this.hvac.getTemperature();
+    this.log.debug('getTargetTemperature', temperature);
+    cb(null, convertFahrenheitToCelsius(temperature));
 };
 
-Thermostat.prototype.setTargetTemperature = function(val, cb) {
-	this.log(val);
-    this.storage.setItemSync(this.name + '&' + 'TargetTemperature', val);
-	this.storage.setItemSync(this.name + '&' + 'CurrentTemperature', val);
-	this.service.setCharacteristic(Characteristic.CurrentTemperature, val);
+Thermostat.prototype.setTargetTemperature = function (temperature, cb) {
+    const temperatureInFahrenheit = convertCelsiusToFahrenheit(temperature, this.minTemperature, this.maxTemperature);
+    this.log.debug('setTargetTemperature', temperatureInFahrenheit);
+    this.hvac.sendTemperature(temperatureInFahrenheit, _ => {
+        this.log.debug('Sent Command', 'sendTemperature', temperatureInFahrenheit);
+    }, err => {
+        this.log.error('sendTemperature Error', err);
+    });
     cb();
 };
 
-Thermostat.prototype.getTemperatureDisplayUnits = function(cb) {
+Thermostat.prototype.getTemperatureDisplayUnits = function (cb) {
+    this.log.debug('getTemperatureDisplayUnits', this.temperatureDisplayUnits);
     cb(null, this.temperatureDisplayUnits);
 };
 
-Thermostat.prototype.setTemperatureDisplayUnits = function(val, cb) {
-	this.log(val);
-    this.storage.setItemSync(this.name + '&' + 'TemperatureDisplayUnits', val);
-    this.temperatureDisplayUnits = val;
+Thermostat.prototype.setTemperatureDisplayUnits = function (displayUnits, cb) {
+    this.log.debug('setTemperatureDisplayUnits', displayUnits);
+    this.temperatureDisplayUnits = displayUnits;
     cb();
 };
 
-Thermostat.prototype.getName = function(cb) {
+Thermostat.prototype.getName = function (cb) {
+    this.log.debug('getName', this.name);
     cb(null, this.name);
 };
 
@@ -127,6 +141,46 @@ Thermostat.prototype.getServices = function () {
     this.service
         .getCharacteristic(Characteristic.Name)
         .on('get', this.getName.bind(this));
-		
-	return [this.informationService, this.service];
+
+    return [this.informationService, this.service];
 };
+
+function getModeHelper(power, mode) {
+    return power === 'off' ? 'off' : mode;
+}
+
+function convertCelsiusToFahrenheit(temperature, minTemperature, maxTemperature) {
+    return Math.min(Math.max(Math.round(temperature * 9 / 5 + 32), minTemperature), maxTemperature);
+}
+
+function convertFahrenheitToCelsius(temperature) {
+    return Math.round((temperature - 32) * 5 / 9);
+}
+
+function convertHeatingCoolingStateToMode(state) {
+    switch (state) {
+        case 1:
+            return 'heat';
+        case 2:
+            return 'cool';
+        case 3:
+            return 'auto';
+        case 0:
+        default:
+            return 'off';
+    }
+}
+
+function convertModeToHeatingCoolingState(mode) {
+    switch (mode) {
+        case 'heat':
+            return 1;
+        case 'cool':
+            return 2;
+        case 'auto':
+            return 3;
+        case 'off':
+        default:
+            return 0;
+    }
+}
